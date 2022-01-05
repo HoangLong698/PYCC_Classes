f034550f594d842b5fcf4b6996ced5574ef5b226
HoangLong698 <tranhuuhoanglong248@gmail.com>
2022-01-05 12:25:41 +0700

    Add read, increment number served customer method, number served attribute

--- restaurant.py
+++ restaurant.py
@@ -5,6 +5,7 @@ class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
     
     # Description restaurant
     def describe_restaurant(self):
@@ -15,10 +16,32 @@ class Restaurant:
     def open_restaurant(self):
         print(f"{self.restaurant_name} restaurant is Open !!!")
 
+    def set_number_served(self, number_served):
+        # Set number restaurant served
+        if number_served >= self.number_served:
+            self.number_served = number_served
+        else:
+            print("the customer served can't negative")
+    
+    def increment_number_served(self, number_served):
+        if number_served >= 0:
+            self.number_served += number_served
+        else:
+            print("The increment served number can't negative")
+
+    def read_number_served(self):
+        print(f"{self.restaurant_name} served {self.number_served} customers.")
+
 my_restaurant = Restaurant("Hoang Long", 'seafood')
 your_restaurant = Restaurant("Da Sac Mau", 'BBQ')
 his_restaurant = Restaurant("Nam Trung Bo", 'Mon Viet')
 
 my_restaurant.describe_restaurant()
-your_restaurant.describe_restaurant()
-his_restaurant.describe_restaurant()
+# your_restaurant.describe_restaurant()
+# his_restaurant.describe_restaurant()
+
+my_restaurant.read_number_served()
+my_restaurant.set_number_served(10)
+my_restaurant.read_number_served()
+my_restaurant.increment_number_served(-10)
+my_restaurant.read_number_served()
