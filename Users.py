815c2b23f8cc413224e9ff677876245a029e4349
HoangLong698 <tranhuuhoanglong248@gmail.com>
2022-01-05 12:52:29 +0700

    Add login attemps attribute, reset and increment login method

--- Users.py
+++ Users.py
@@ -9,6 +9,7 @@ class Users:
         self.height = height
         self.weight = weight
         self.name = first_name + ' ' +last_name
+        self.login_attemps = 0
 
     # Creat a method describe user
     def describe_user(self):
@@ -19,6 +20,16 @@ class Users:
     # Create a method welcome user
     def greet_user(self):
         print(F"Welcome to page {self.name}")
+    
+    def increment_login_attempts(self):
+        # Add increment login by 1
+        self.login_attemps += 1
+
+    def reset_login_attempts(self):
+        self.login_attemps = 0
+    
+    def print_login_attemps(self):
+        print(f"{self.name} login attemps is {self.login_attemps}")
 
 user1 = Users('Long', 'Tran', 21, 160, 60)
 user2 = Users('John', 'Nguyen', 21, 175, 70)
@@ -28,3 +39,10 @@ user2.describe_user()
 
 user1.greet_user()
 user2.greet_user()
+
+print(f"User {user1.name} login attemps: {user1.login_attemps}")
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.print_login_attemps()
+user1.reset_login_attempts()
+user1.print_login_attemps()
