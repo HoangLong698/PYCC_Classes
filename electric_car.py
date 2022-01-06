5e9925c3b6f6c14a0c77c19b45da661c1587ce87
HoangLong698 <tranhuuhoanglong248@gmail.com>
2022-01-06 12:57:27 +0700

    ElectricCar, Battery Class

--- electric_car.py
+++ electric_car.py
@@ -1,39 +1,6 @@
-class Car:
-    # A simple attempt to represent a car.
+""" A set of classes that can be used to represent electric cars."""
 
-    def __init__(self, manufacturer, model, year) -> None:
-        # Initilize attributes to describe a car.
-        self.manufacturer = manufacturer
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-
-    def get_descriptive_name(self):
-        # Return a neatly formatted descriptive name.
-        long_name = f"{self.year} {self.manufacturer} {self.model}"
-        return long_name.title()
-    
-    def read_odometer(self):
-        # Print a statement showing the car's mileage.
-        print(f"This car has {self.odometer_reading} miles on it.")
-
-    def update_odometer(self, milege):
-        # Set the odometer reading to the give value.
-        # Reject the change if it attempts to roll the odometer back.
-        if milege >= self.odometer_reading:
-            self.odometer_reading = milege
-        else:
-            print("You can't roll back an odometer!!!.")
-    
-    def increment_odometer(self, milege):
-        # Add the given amount to the odometer reading.
-        if milege >= 0:
-            self.odometer_reading += milege
-        else:
-            print("Milege can't negative number")
-    
-    def fill_gas_tank(self):
-        print("The car has a full tank of gas")
+from car import Car
 
 class Battery:
     """ A simple attempt to model a battery for an electric car."""
@@ -76,24 +43,3 @@ class ElectricCar(Car):
     def fill_gas_tank(self):
         """ Electric cars don't have gas tank."""
         print("This car doesn't need a gas tank")
-
-
-# my_bmw = Car('BMW', 'i8','2021')
-# print(my_bmw.get_descriptive_name())
-# my_bmw.fill_gas_tank()
-
-# my_tesla = ElectricCar('tesla', 'model s', 2019)
-# print(my_tesla.get_descriptive_name())
-# # my_tesla.read_odometer()
-# # my_tesla.update_odometer(20)
-# # my_tesla.read_odometer()
-# my_tesla.battery.describe_battery()
-
-my_vinfast = ElectricCar('Vinfast', 'VF8', 2021)
-print(my_vinfast.get_descriptive_name())
-my_vinfast.battery.describe_battery()
-# my_vinfast.fill_gas_tank()
-my_vinfast.battery.get_range()
-my_vinfast.battery.upgrade_battery()
-my_vinfast.battery.describe_battery()
-my_vinfast.battery.get_range()
