02fd9b30347aace6b19d57a12237133e6365d195
HoangLong698 <tranhuuhoanglong248@gmail.com>
2022-01-06 11:38:55 +0700

    Add Admin Class

--- Users.py
+++ Users.py
@@ -31,18 +31,33 @@ class Users:
     def print_login_attemps(self):
         print(f"{self.name} login attemps is {self.login_attemps}")
 
+class Admin(Users):
+    # Initialize Admin Class inheritance from Users Class
+    def __init__(self, first_name, last_name, age, height, weight) -> None:
+        super().__init__(first_name, last_name, age, height, weight)
+        self.privileges = ['can add post', 'can delete post', 'can ban user', 'can grant permission']
+    
+    # Show the privileges of instance of Admin class
+    def show_privileges(self):
+        print(f"The privileges of Administrator {self.name}: ")
+        for privilege in self.privileges:
+            print(f"+ {privilege}")
+
 user1 = Users('Long', 'Tran', 21, 160, 60)
 user2 = Users('John', 'Nguyen', 21, 175, 70)
+user3 = Admin('John', 'Le', 23, 165, 80)
 
-user1.describe_user()
-user2.describe_user()
+# user1.describe_user()
+# user2.describe_user()
+user3.describe_user()
+user3.show_privileges()
 
-user1.greet_user()
-user2.greet_user()
+# user1.greet_user()
+# user2.greet_user()
 
-print(f"User {user1.name} login attemps: {user1.login_attemps}")
-user1.increment_login_attempts()
-user1.increment_login_attempts()
-user1.print_login_attemps()
-user1.reset_login_attempts()
-user1.print_login_attemps()
+# print(f"User {user1.name} login attemps: {user1.login_attemps}")
+# user1.increment_login_attempts()
+# user1.increment_login_attempts()
+# user1.print_login_attemps()
+# user1.reset_login_attempts()
+# user1.print_login_attemps()
