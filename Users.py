cf9c79c5c1a60a6b02a8b4681220f43702399794
HoangLong698 <tranhuuhoanglong248@gmail.com>
2022-01-06 13:09:32 +0700

    Users Class

--- Users.py
+++ Users.py
@@ -1,6 +1,5 @@
 class Users:
-    # Create Class Users
-
+    # Create Class User
     # Initialize first_name and last_name
     def __init__(self, first_name, last_name, age, height, weight) -> None:
         self.first_name = first_name
@@ -31,40 +30,19 @@ class Users:
     def print_login_attemps(self):
         print(f"{self.name} login attemps is {self.login_attemps}")
 
-class Privileges:
-    # Initialize Privileges Class
-    def __init__(self):
-        self.privileges = ['can add post', 'can delete post', 'can ban user', 'can grant permission']
+# class Privileges:
+#     # Initialize Privileges Class
+#     def __init__(self):
+#         self.privileges = ['can add post', 'can delete post', 'can ban user', 'can grant permission']
 
-    def show_privileges(self):
-        print(f"The privileges of Administrator: ")
-        for privilege in self.privileges:
-            print(f"+ {privilege}")
+#     # Show the privileges of instance of Admin class
+#     def show_privileges(self):
+#         print(f"The privileges of Administrator: ")
+#         for privilege in self.privileges:
+#             print(f"+ {privilege}")
 
-class Admin(Users):
-    # Initialize Admin Class inheritance from Users Class
-    def __init__(self, first_name, last_name, age, height, weight) -> None:
-        super().__init__(first_name, last_name, age, height, weight)
-        self.privileges = Privileges()
-    
-    # Show the privileges of instance of Admin class
-    
-
-user1 = Users('Long', 'Tran', 21, 160, 60)
-user2 = Users('John', 'Nguyen', 21, 175, 70)
-user3 = Admin('John', 'Le', 23, 165, 80)
-
-# user1.describe_user()
-# user2.describe_user()
-user3.describe_user()
-user3.privileges.show_privileges()
-
-# user1.greet_user()
-# user2.greet_user()
-
-# print(f"User {user1.name} login attemps: {user1.login_attemps}")
-# user1.increment_login_attempts()
-# user1.increment_login_attempts()
-# user1.print_login_attemps()
-# user1.reset_login_attempts()
-# user1.print_login_attemps()
+# class Admin(Users):
+#     # Initialize Admin Class inheritance from Users Class
+#     def __init__(self, first_name, last_name, age, height, weight) -> None:
+#         super().__init__(first_name, last_name, age, height, weight)
+#         self.privileges = Privileges()
