ca246faa4c79f3f9a695cfb0a22163c5ae58e9cd
HoangLong698 <tranhuuhoanglong248@gmail.com>
2022-01-06 13:03:01 +0700

    Instance of Restaurant,IceCreamStand Class

---
+++ my_restaurant.py
@@ -0,0 +1,22 @@
+from restaurant import Restaurant,IceCreamStand
+
+# my_restaurant = Restaurant("Hoang Long", 'Seafood')
+# your_restaurant = Restaurant("Da Sac Mau", 'BBQ')
+# his_restaurant = Restaurant("Nam Trung Bo", 'Mon Viet')
+
+# my_restaurant.describe_restaurant()
+# your_restaurant.describe_restaurant()
+# his_restaurant.describe_restaurant()
+
+# my_restaurant.read_number_served()
+# my_restaurant.set_number_served(10)
+# my_restaurant.read_number_served()
+# my_restaurant.increment_number_served(-10)
+# my_restaurant.read_number_served()
+
+my_icecream_res = IceCreamStand('Trang Tien','Ice Cream')
+my_icecream_res.describe_restaurant()
+my_icecream_res.get_flavors()
+my_icecream_res.read_number_served()
+my_icecream_res.set_number_served(10)
+my_icecream_res.read_number_served()
